a75cbef4bc144b7d61a67e54ba5db94d8c00470e
TrungCute <htmtrung2004@gmail.com>
2024-04-21 14:08:00 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,26 +3,26 @@ import withPWAInit from "@ducanh2912/next-pwa"
 const withPWA = withPWAInit({
   dest: "public",
   extendDefaultRuntimeCaching: true,
-//   workboxOptions: {
-//     runtimeCaching: [
-//       {
-//         urlPattern: ({ url }) => {
-//             return url.pathname.startsWith('/products')
-//         },
-//         handler: "CacheFirst",
-//         options: {
-//             cacheName: "products",
-//             cacheableResponse: {
-//                 statuses: [0, 200]
-//             }
-//             // expiration: {
-//             //     maxEntries: 4,
-//             //     maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-//             // },
-//         },
-//       }
-//     ],
-//   },
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => {
+            return url.pathname.startsWith('/category')
+        },
+        handler: "CacheFirst",
+        options: {
+            cacheName: "products",
+            cacheableResponse: {
+                statuses: [0, 200]
+            }
+            // expiration: {
+            //     maxEntries: 4,
+            //     maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+            // },
+        },
+      }
+    ],
+  },
 })
 
 export default withPWA({
