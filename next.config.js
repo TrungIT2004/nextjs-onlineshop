3117b36a6eb86e9f0b610ffa49967f0a6cefc8ec
TrungCute <htmtrung2004@gmail.com>
2024-04-21 14:20:59 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,3 +3,13 @@ module.exports = {
     domains: ['images.officex.click'], // Replace with your domain
   },
 };
+
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
+  reactStrictMode: true,
+});
