3b39c1ddc939119d9b94f7e1a2aee3478d11ab1b
TrungCute <htmtrung2004@gmail.com>
2024-04-21 14:25:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  images: {
-    domains: ['images.officex.click'], // Replace with your domain
-  },
-};
+const withPWA = require("@ducanh2912/next-pwa")
 
+module.exports = withPWA({
+  pwa: {
+    dest: "public", // Location for generated service worker and manifest files
+    disable: process.env.NODE_ENV === "development", // Disable PWA in development
+    // Add other PWA configuration options here (optional)
+  },
+  images: {
+    domains: ["images.officex.click"], // Optimize images from your domain
+  },
+  reactStrictMode: true, // Enable React Strict Mode for better development experience
+})
