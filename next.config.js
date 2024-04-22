26bffb005de960b7c06b34a7fd920bbf9fb9df80
TrungCute <htmtrung2004@gmail.com>
2024-04-22 15:25:33 +0700

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -3,3 +3,31 @@ module.exports = {
     domains: ['images.officex.click'], // Replace with your domain
   },
 };
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => {
+            return url.pathname.startsWith('/category')
+        },
+        handler: "CacheFirst",
+        options: {
+            cacheName: "products",
+            cacheableResponse: {
+                statuses: [0, 200]
+            }
+            // expiration: {
+            //     maxEntries: 4,
+            //     maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+            // },
+        }
+      }
+    ],
+  },
+});
+
+module.exports = withPWA({
+  // Your Next.js config
+});
